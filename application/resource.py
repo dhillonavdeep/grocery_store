aa87e66567c593a9d57946113137fe6f69b656c6
dhillonavdeep <navdeep@Navdeep>
2024-01-03 13:30:49 +0530

    validation added

--- application/resource.py
+++ application/resource.py
@@ -1,16 +1,24 @@
-from flask_restful import Resource,Api,reqparse
+from flask_restful import Resource,Api,reqparse,fields, marshal_with
 from .models import Product,db
 
 api=Api(prefix='/api')
 
 
 parser=reqparse.RequestParser()
-parser.add_argument('name', type=str, help='Please provide a valid string for the name.')
-parser.add_argument('description', type=str, help='Please provide a valid string for the description.')
+parser.add_argument('name', type=str, help='Please provide a valid string for the name.',required=True)
+parser.add_argument('description', type=str, help='Please provide a valid string for the description.',required=True)
 parser.add_argument('section', type=str, help='Please provide a valid string for the section.')
-parser.add_argument('quantity', type=int, help='Please provide a valid integer for the quantity.')
-parser.add_argument('amount', type=int, help='Please provide a valid integer for the amount.')
+parser.add_argument('quantity', type=int, help='Please provide a valid integer for the quantity.',required=True)
+parser.add_argument('amount', type=int, help='Please provide a valid integer for the amount.',required=True)
 
+product_details_fields = {
+    'id':   fields.String,
+    'name':   fields.String,
+    'description':  fields.String,
+    'section':  fields.String,
+    'amount': fields.Integer,
+    'quantity':fields.Integer
+}
 
 class ProductDetails(Resource):
     def get(self):
